ed6d0e10de1443d0219578fd18a2ad69c6b1e45a
Sean Collins <sean.collins@outlook.com>
2025-04-27 20:13:17 -0700

    Add some argument parsing and interactivity

--- main.c
+++ main.c
@@ -1,9 +1,19 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include "parsexec.h"
+
+static char input[100] = "look around";
+
+static bool getInput(void)
+{
+    printf("\n..> ");
+    return fgets(input, sizeof input, stdin) != NULL;
+}
 
 int main()
 {
    printf("Welcome to Little Cave Adventure.\n");
-   printf("It is very dark in here.\n");
+   while (parseAndExecute(input) && getInput());
    printf("\nBye!\n");
    return 0;
 }
