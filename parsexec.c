13a482c0aeb050c609c5841363a86a39dc1524db
Sean Collins <sean.collins@outlook.com>
2025-05-01 19:18:36 -0700

    Add logic for navigating to locations and update stubs

--- parsexec.c
+++ parsexec.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+#include "location.h"
 
 bool parseAndExecute(char *input)
 {
@@ -14,11 +15,11 @@ bool parseAndExecute(char *input)
       }
       else if (strcmp(verb, "look") == 0)
       {
-         printf("It is very dark in here.\n");
+         executeLook(noun);
       }
       else if (strcmp(verb, "go") == 0)
       {
-         printf("It's too dark to go anywhere.\n");
+         executeGo(noun);
       }
       else
       {
